ba7369a32e51e403e89ddefa39143a2f6b34ce30
Justin Kaseman <justinkaseman@live.com>
2020-09-25 15:40:19 -0700

    feat: rinkeby ABIs

---
+++ src/abi/index.ts
@@ -0,0 +1,6 @@
+import controllerAbi from './controller'
+import registrarAbi from './registrar'
+import registryAbi from './registry'
+import resolverAbi from './resolver'
+
+export { controllerAbi, registrarAbi, registryAbi, resolverAbi }
