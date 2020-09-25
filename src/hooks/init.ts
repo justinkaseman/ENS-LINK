84237c9e801942579d96379330cfa6dbe6aafa21
Justin Kaseman <justinkaseman@live.com>
2020-09-25 15:41:59 -0700

    feat: Run configuration as an 'init' hook

---
+++ src/hooks/init.ts
@@ -0,0 +1,21 @@
+import { Hook } from '@oclif/config'
+import { readConfig } from '../utils/config'
+import Configure from '../commands/configure'
+
+/*
+Init hook:
+Ensures that the user is configured correctly before running any command
+*/
+
+export const hook: Hook<'init'> = async function (options) {
+    // This hook is redundant if the user is running the configure command
+    if (options.id === 'configure') return
+
+    try {
+        await readConfig(options.config.configDir, 'config.json')
+    }
+    catch (error) {
+        this.log('Welcome to ENS-LINK! Please configure')
+        await Configure.run()
+    }
+}
